f9653c683b6d01f91ed82b618fecab2582cb5318
pranjalsorte <pranjalsorte.8305@gmail.com>
2026-01-11 20:40:30 +0530

    Remove tensorflow for Streamlit Cloud compatibility

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from tensorflow.keras.models import load_model
-from sklearn.preprocessing import MinMaxScaler
+from keras.models import load_model
+
 
 # -----------------------------------------------
 # App Title & Description
