4c2e558bfa81226a58a5427048d37b992f9f15a1
pranjalsorte <pranjalsorte.8305@gmail.com>
2026-01-11 20:50:26 +0530

    changes in tensorflow

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
 from keras.models import load_model
 
 
