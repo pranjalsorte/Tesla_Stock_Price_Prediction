87225a2ee358f76d96a1c768a8fbc65b742af24b
pranjalsorte <pranjalsorte.8305@gmail.com>
2026-01-12 12:13:09 +0530

    added keras

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
+from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
-
 
 
 # -----------------------------------------------
